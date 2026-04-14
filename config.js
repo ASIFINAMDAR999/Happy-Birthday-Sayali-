a4498be18d01ec773fda1561f31cb08bbdd145e4
ASIFINAMDAR999 <inamdarasif678@gmail.com>
2026-04-14 19:40:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,17 @@ const CONFIG = {
     {
       type: "chatbox",
       message:
-        "Happy Birthday, my love ❤️ To my dearest Sayali, I wish you the most beautiful birthday and an amazing year ahead filled with happiness, love, and everything your heart desires, you truly deserve all the joy in the world, every moment with you is so special to me and I’m so grateful to have you in my life, I promise to always stand by you and love you more with each passing day, I love you so much ❤️✨",
+        "
+
+Happy Birthday to the most precious part of my life my love Sayali ❤️🎂
+At this moment I just want to thank god for bringing you to me because meeting you was not planned but loving you became the most beautiful thing in my life 💖 you are not just someone special you are my feeling my peace and the part of my heart I never want to lose
+You are my good luck charm  since you came into my life everything feels better and more meaningful
+I may not always express it perfectly but you have changed me in the best way possible and having you in my life means everything to me 🥺❤️
+I promise I will always stand by you in every situation I will respect you trust you and never give you a reason to doubt my love I will protect your heart take care of your smile and keep choosing you every single day no matter what happens I am never letting you go and I will never stop loving you. my love for you is infinite i told you yesterday not 2 3 4 5 its ♾️❤️.you are my first love i will make it last i loveing you to marry you i wan be your life partner 👩‍❤️‍💋‍👨 I wanna spend my whole life loving you ❤️  i am 100% sure 
+You are my happiness my peace my forever and on your special day I just wish your smile never fades and your life is filled with everything you deserve and you wished (mr ai is included free for you in that wishes 🫣) ✨
+I love you more than I can ever explain in words my love for you is infinite my love ❤️😘
+I love youuuuuu ♾️ ❤️
+Once again, wish you many many happy returns of the day my love ❤️",
       buttonText: "Send",
     },
     {
